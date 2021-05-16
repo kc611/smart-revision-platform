90c1df1344b09a03333989d1e472f3c36487d9ca
kc611 <ckaustubhm06@gmail.com>
2021-05-16 18:14:25 +0530

    Added login UI

--- public/login_page_utilities.js
+++ public/login_page_utilities.js
@@ -1,66 +1,11 @@
-$("#signup").click(function () {
-  $("#first").fadeOut("fast", function () {
-    $("#second").fadeIn("fast");
-  });
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
 });
 
-$("#signin").click(function () {
-  $("#second").fadeOut("fast", function () {
-    $("#first").fadeIn("fast");
-  });
-});
-
-$(function () {
-  $("form[name='login']").validate({
-    rules: {
-      email: {
-        required: true,
-        email: true,
-      },
-      password: {
-        required: true,
-      },
-    },
-    messages: {
-      email: "Please enter a valid email address",
-
-      password: {
-        required: "Please enter password",
-      },
-    },
-    submitHandler: function (form) {
-      form.submit();
-    },
-  });
-});
-
-$(function () {
-  $("form[name='registration']").validate({
-    rules: {
-      firstname: "required",
-      lastname: "required",
-      email: {
-        required: true,
-        email: true,
-      },
-      password: {
-        required: true,
-        minlength: 5,
-      },
-    },
-
-    messages: {
-      firstname: "Please enter your firstname",
-      lastname: "Please enter your lastname",
-      password: {
-        required: "Please provide a password",
-        minlength: "Your password must be at least 5 characters long",
-      },
-      email: "Please enter a valid email address",
-    },
-
-    submitHandler: function (form) {
-      form.submit();
-    },
-  });
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
 });
