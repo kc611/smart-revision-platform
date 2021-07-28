0c406f94ba7bc02652e8f122d1fd5873fa25a792
kc611 <ckaustubhm06@gmail.com>
2021-07-28 22:09:30 +0530

    Added admin panel

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const mongo = require("mongodb");
-const uri = require("../config/keys").MongoURI;
 const User = require("../models/User");
 const utils = require('./utils')
+const mongo = require("mongodb");
+const uri = require("../config/keys").MongoURI;
+const client = new mongo.MongoClient(uri,{ useUnifiedTopology: true });
 
 router.post('/register', (req, res) => {
     var curr_username = req.body.username;
@@ -27,7 +28,87 @@ router.post('/register', (req, res) => {
 });
 
 router.get('/dashboard', utils.continue_if_admin, (req, res) => {
-    res.render("admin_pages/dashboard",{layout:'./blank_base',title:"Admin Dashboard"});
-})
+    res.render("admin_pages/dashboard_main",{layout:'./admin_pages/dashboard_base',title:"Admin Dashboard"});
+});
+
+
+router.get('/verify-users', utils.continue_if_admin, async (req, res) => {
+    await client.connect();
+
+    const org_database = client.db("test");
+    const user_collections = org_database.collection("users");
+    
+    var resp_data = []
+    await user_collections.find({organization:req.user.organization, verified:false}).forEach(
+        function(curr_resp) { 
+            var curr_resp_data = {
+                "username":curr_resp.username,
+                "email":curr_resp.email
+            } 
+            resp_data.push(curr_resp_data);
+         }
+    );
+
+    res.render("admin_pages/dashboard_verify",{layout:'./admin_pages/dashboard_base',title:"Verify Student Profile", resp_data:resp_data});
+});
+
+
+router.get('/inventory', utils.continue_if_admin, async (req, res) => {
+    await client.connect();
+
+    const org_database = client.db(req.user.organization.replace(" ",""));
+    const user_collections = org_database.collection("subjects");
+    
+    var resp_data = []
+    await user_collections.find({}).forEach(
+        function(curr_resp) { 
+            var curr_resp_data = {
+                "subject_name":curr_resp.subject_name,
+                "subject_code":curr_resp.subject_code
+            } 
+            resp_data.push(curr_resp_data);
+         }
+    );
+
+    res.render("admin_pages/dashboard_notes",{layout:'./admin_pages/dashboard_base',title:"Upload Notes",resp_data:resp_data});
+});
+
+router.get('/verify', utils.continue_if_admin, async (req, res) => {
+    var unverified_user = req.query.user;
+
+    await client.connect();
+
+    const org_database = client.db("test");
+    const user_collections = org_database.collection("users");
+    const updateDoc = {
+        $set: {
+          verified:true
+        },
+      };
+
+    user_collections.updateOne({username:unverified_user, verified:false},updateDoc);
+
+
+    res.redirect("verify-users");
+});
+
+router.post('/add-subject', utils.continue_if_admin, async (req, res) => {
+    var subject_name = req.body.subject_name;
+    var subject_code = req.body.subject_code;
+
+    await client.connect();
+
+    const org_database = client.db(req.user.organization.replace(" ",""));
+    const sub_collections = org_database.collection("subjects");
+
+    const responseObject = {
+        "subject_name":subject_name,
+        "subject_code":subject_code.replace(" ","")
+    }
+    
+    sub_collections.insertOne(responseObject);
+
+    res.redirect("inventory");
+});
 
 module.exports = router;
